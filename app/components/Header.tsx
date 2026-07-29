9789facf6acf8b2e96f7cf2fa10e9c36e6865c9e
Malak Abu-Omar <malakmuayad15@gmail.com>
2026-07-29 13:52:33 +0300

    Implement Header style and interactivity

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,9 @@
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import collapseIcon from "../assets/collapse-icon.svg";
+import lightModeIcon from "../assets/light-mode-icon.svg";
+import darkModeIcon from "../assets/dark-mode-icon.svg";
+import { ThemeContext } from "~/context/ThemeContext";
 
 export default function Header({
   onCollapseClick,
@@ -8,10 +12,15 @@ export default function Header({
 }) {
   const user = useContext(UserContext)?.user;
   const fullName = user?.firstName + " " + user?.lastName;
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  function toggleTheme() {
+    setTheme(theme === "Light" ? "Dark" : "Light");
+  }
 
   return (
     <header className=" relative flex gap-2 justify-between mt-0.5 border-b border-gray-500 p-1">
-      <div className="flex justify-evenly shrink-0 gap-0.5">
+      <div className="flex justify-evenly shrink-0 gap-0.5 mb-0.5">
         <button
           id="btnCollapse"
           data-collapse="false"
@@ -19,28 +28,34 @@ export default function Header({
           onClick={onCollapseClick}
         >
           <img
-            src="src/assets/collapse.svg"
+            src={collapseIcon}
             alt="Collapse icon"
-            className="h-5 w-5 pr-1"
+            className="h-7 w-7 pr-1"
           />
         </button>
         <input
           type="search"
-          placeholder="Search anything..."
-          className="bg-blue-800/30 p-1.5 rounded-md appearance-none outline-none border border-transparent focus:outline-none focus:ring-0"
+          placeholder="Search tasks..."
+          className="border-[1.75px] border-border-color rounded-btn bg-primary-light p-1 focus:outline-primary w-full"
         />
       </div>
-      <div className="flex items-center gap-4">
-        <button className="md:after:content-['_|'] md:after:text-gray-300 cursor-pointer">
-          🔔
-        </button>
-        <div className="flex items-center gap-1 md:gap-2">
+      <div className="flex items-center -gap-1">
+        <button className=" cursor-pointer flex gap-1" onClick={toggleTheme}>
           <img
-            src={user?.profilePictureURL ?? "src/assets/default-avatar.png"}
-            alt={"User Avatar"}
+            className="w-5 h-5 top-1"
+            src={theme === "Dark" ? lightModeIcon : darkModeIcon}
+            alt={theme === "Dark" ? "Light mode icon" : "Dark mode icon"}
+            loading="eager"
+          />
+          <span>|</span>
+        </button>
+        <div className="flex items-center -gap-1">
+          <img
+            src={user?.profilePictureURL}
+            alt="User Avatar"
             className="object-cover rounded-full w-8 h-8"
           />
-          <span className="text-center">{fullName ?? "Unknown"}</span>
+          <p className="text-center text-sm">{fullName ?? "Unknown"}</p>
         </div>
       </div>
     </header>
