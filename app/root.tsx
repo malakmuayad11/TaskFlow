46fa26cc1b968e25a94e032608f19f7ce9c91557
Malak Abu-Omar <malakmuayad15@gmail.com>
2026-08-02 21:57:36 +0300

    Add website favicon

--- app/root.tsx
+++ app/root.tsx
@@ -15,6 +15,7 @@ import ThemeProvider from "./context/ThemeProvider";
 import { ViewProvider } from "./context/ViewProvider";
 
 export const links: Route.LinksFunction = () => [
+  // Google Fonts
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
@@ -24,6 +25,26 @@ export const links: Route.LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  },
+
+  // Favicons
+  { rel: "icon", href: "public/favicon.ico" },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "public/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "public/favicon-16x16.png",
+  },
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "public/apple-touch-icon.png",
   },
 ];
 
