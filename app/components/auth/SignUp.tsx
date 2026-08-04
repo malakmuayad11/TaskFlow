46c848bffb16217f144687cef74e85286c99de24
Malak Abu-Omar <malakmuayad15@gmail.com>
2026-08-04 21:15:24 +0300

    Find issue in phone

--- app/components/auth/SignUp.tsx
+++ app/components/auth/SignUp.tsx
@@ -1,31 +1,25 @@
-import { useRef, useState, useContext } from "react";
+import { useRef, useState, useEffect, useContext } from "react";
 import type { FormEvent } from "react";
-import { useNavigate } from "react-router";
-
 import { addUser } from "../../services/indexedDB/userService";
-import { EMAIL_REGX, PASSWORD_REGX } from "~/services/validation";
-import { useToast } from "~/hooks/useToast";
-
+import { useNavigate } from "react-router";
 import Input from "../shared/Input";
 import FileInput from "../shared/FileInput";
 import Button from "../shared/Button";
-import Toast from "../shared/Toast";
-
+import { EMAIL_REGX, PASSWORD_REGX } from "~/services/validation";
+import { useToast } from "~/hooks/useToast";
 import { ThemeContext } from "~/context/ThemeContext";
-
+import Toast from "../shared/Toast";
 import { fileToBase64 } from "~/services/base64";
 import profilePicturePlaceholder from "../../assets/profilePicturePlaceholder.svg";
 
 export default function SignUp() {
-  const { theme } = useContext(ThemeContext);
-
+  const theme = useContext(ThemeContext).theme;
   const firstName = useRef<HTMLInputElement | null>(null);
   const lastName = useRef<HTMLInputElement | null>(null);
   const email = useRef<HTMLInputElement | null>(null);
   const password = useRef<HTMLInputElement | null>(null);
   const confirmPassword = useRef<HTMLInputElement | null>(null);
   const profilePictureURL = useRef<HTMLInputElement | null>(null);
-
   const [emailExists, setEmailExists] = useState(false);
   const [showToast, setShowToast] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -38,9 +32,54 @@ export default function SignUp() {
     confirmPassword: false,
   });
 
-  const navigate = useNavigate();
+  const navigator = useNavigate();
 
   useToast(showToast, setShowToast);
+
+  // async function signUp(event: FormEvent<HTMLFormElement>) {
+  //   event.preventDefault();
+
+  //   // ensure all refs are available before accessing their values
+  //   if (
+  //     !firstName.current ||
+  //     !lastName.current ||
+  //     !email.current ||
+  //     !password.current ||
+  //     !confirmPassword.current ||
+  //     !profilePictureURL.current
+  //   )
+  //     return;
+
+  //   if (!Object.values(formValidity).every((f) => f === true)) return;
+
+  //   // provide default profile picture
+  //   const file = profilePictureURL.current.files?.[0];
+  //   const profilePicture = file
+  //     ? await fileToBase64(file)
+  //     : profilePicturePlaceholder;
+
+  //   try {
+  //     await addUser({
+  //       firstName: firstName.current.value,
+  //       lastName: lastName.current.value,
+  //       email: email.current.value,
+  //       password: password.current.value,
+  //       profilePictureURL: profilePicture,
+  //     });
+  //     alert("going to the main page1");
+
+  //     setEmailExists(false);
+  //     setShowToast(true);
+  //     alert("going to the main page2");
+  //     setTimeout(() => {
+  //       navigator("/"); // go to start page
+  //     }, 2000);
+  //   } catch {
+  //     alert("error occurred");
+  //     setEmailExists(true);
+  //     email.current.className += "outline-2 outline-red-500";
+  //   }
+  // }
 
   async function signUp(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -85,7 +124,7 @@ export default function SignUp() {
 
       alert("7: user added");
 
-      navigate("/");
+      navigator("/");
     } catch (error) {
       alert(
         "ERROR: " + (error instanceof Error ? error.message : String(error)),
@@ -97,22 +136,14 @@ export default function SignUp() {
 
   return (
     <div
-      className={`flex justify-center items-center min-h-screen w-full ${
-        theme === "Light" ? "bg-slate-50" : "bg-bg-main-dark"
-      } px-4`}
+      className={`flex justify-center items-center min-h-screen w-full ${theme === "Light" ? "bg-slate-50" : "bg-bg-main-dark"} px-4`}
     >
       <form
         onSubmit={signUp}
-        className={`grid grid-cols-1 md:grid-cols-2 gap-2 w-full max-w-xl ${
-          theme === "Light"
-            ? "bg-bg-main border-border-color"
-            : "bg-bg-surface-dark border-border-color-dark shadow-primary/30"
-        } p-2 md:p-4 border rounded-btn shadow-xl`}
+        className={`grid grid-cols-1 md:grid-cols-2 gap-2 w-full max-w-xl ${theme === "Light" ? "bg-bg-main border-border-color" : "bg-bg-surface-dark border-border-color-dark shadow-primary/30"} p-2 md:p-4 border rounded-btn shadow-xl`}
       >
         <h2
-          className={`text-2xl col-span-1 md:col-span-2 md:text-4xl font-semibold ${
-            theme === "Light" ? "text-primary-dark" : "text-primary-light"
-          }`}
+          className={`text-2xl col-span-1 md:col-span-2 md:text-4xl font-semibold ${theme === "Light" ? "text-primary-dark" : "text-primary-light"}`}
         >
           Create Your Account
         </h2>
@@ -155,12 +186,11 @@ export default function SignUp() {
               }))
             }
           />
-
-          {emailExists && (
-            <p className="text-red-500 text-sm">
-              This email is already registered.
-            </p>
-          )}
+          <p
+            className={`${emailExists ? "visible" : "hidden"} text-red-500 text-sm`}
+          >
+            This email is already registered. Login instead.
+          </p>
         </div>
 
         <Input
@@ -168,7 +198,7 @@ export default function SignUp() {
           labelName="Password:"
           ref={password}
           validationRegex={PASSWORD_REGX}
-          validationMsg="Password must contain 8 characters, uppercase, lowercase, number and special character."
+          validationMsg="Password must be at least 8 characters and include an uppercase letter, a lowercase letter, a number, and a special character."
           onValidationChange={(valid) =>
             setFormValidity((prev) => ({
               ...prev,
@@ -181,9 +211,9 @@ export default function SignUp() {
           inputType="password"
           labelName="Confirm Password:"
           ref={confirmPassword}
-          onValidate={() =>
-            password.current?.value === confirmPassword.current?.value
-          }
+          onValidate={() => {
+            return password.current?.value === confirmPassword.current?.value;
+          }}
           validationMsg="Passwords must match"
           onValidationChange={(valid) =>
             setFormValidity((prev) => ({
@@ -201,12 +231,11 @@ export default function SignUp() {
         />
 
         <Button
-          content={isSubmitting ? "Creating..." : "Create account"}
+          content="Create account"
           type="submit"
           className="col-span-1 md:col-span-2 z-50"
         />
       </form>
-
       {showToast && <Toast title="Account is created successfully." />}
     </div>
   );
