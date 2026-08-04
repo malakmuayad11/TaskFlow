a8c660ae2e5eabe97e772a84e36f177f42fbae95
Malak Abu-Omar <malakmuayad15@gmail.com>
2026-08-04 20:49:51 +0300

    Find issue in phone

--- app/components/auth/SignUp.tsx
+++ app/components/auth/SignUp.tsx
@@ -65,12 +65,16 @@ export default function SignUp() {
         password: password.current.value,
         profilePictureURL: profilePicture,
       });
+      alert("going to the main page1");
+
       setEmailExists(false);
       setShowToast(true);
+      alert("going to the main page2");
       setTimeout(() => {
         navigator("/"); // go to start page
       }, 2000);
     } catch {
+      alert("error occurred");
       setEmailExists(true);
       email.current.className += "outline-2 outline-red-500";
     }
