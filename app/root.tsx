4a5bb38d18743c37cccca9c0cf09e9d214cf52fd
Malak Abu-Omar <malakmuayad15@gmail.com>
2026-08-04 17:57:13 +0300

    Fix favicon loading issue

--- app/root.tsx
+++ app/root.tsx
@@ -28,23 +28,23 @@ export const links: Route.LinksFunction = () => [
   },
 
   // Favicons
-  { rel: "icon", href: "public/favicon.ico" },
+  { rel: "icon", href: "/favicon.ico" },
   {
     rel: "icon",
     type: "image/png",
     sizes: "32x32",
-    href: "public/favicon-32x32.png",
+    href: "/favicon-32x32.png",
   },
   {
     rel: "icon",
     type: "image/png",
     sizes: "16x16",
-    href: "public/favicon-16x16.png",
+    href: "/favicon-16x16.png",
   },
   {
     rel: "apple-touch-icon",
     sizes: "180x180",
-    href: "public/apple-touch-icon.png",
+    href: "/apple-touch-icon.png",
   },
 ];
 
